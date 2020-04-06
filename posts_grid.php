84a0fa346542b89f1ed61c03aea6e510ca11c117
Justin Tharpe <justin@Justins-MacBook-Pro.local>
2020-04-05 21:49:20 -0400

    Initial Commit

---
+++ posts_grid.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Plugin Name: Posts Grid
+ * Plugin URI: https://overflownetworks.com 
+ * Description: Display posts with image on the left, and text, and description on the right.
+ * Version: 0.1
+ * Author: Justin Tharpe
+ */
+ 
+ function tbare_wordpress_plugin_demo($atts) {
+	$Content = "<style>\r\n";
+	$Content .= "h3.demoClass {\r\n";
+	$Content .= "color: #26b158;\r\n";
+	$Content .= "}\r\n";
+	$Content .= "</style>\r\n";
+	$Content .= '<h3 class="demoClass">Check it out!</h3>';
+	 
+    return $Content;
+}
+
+add_shortcode('tbare-plugin-demo', 'tbare_wordpress_plugin_demo');
