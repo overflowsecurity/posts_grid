7dff1c9721092b5e9daa4c061b3ee00a31ab3ec3
Justin Tharpe <justin@Justins-MacBook-Pro.local>
2020-04-05 21:57:24 -0400

    Test

--- posts_grid.php
+++ posts_grid.php
@@ -14,7 +14,27 @@
 	$Content .= "}\r\n";
 	$Content .= "</style>\r\n";
 	$Content .= '<h3 class="demoClass">Check it out!</h3>';
-	 
+     
+    // the query
+   $the_query = new WP_Query( array(
+    'category_name' => 'news',
+     'posts_per_page' => 3,
+    )); 
+    ?>
+
+    <?php if ( $the_query->have_posts() ) : ?>
+    <?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
+
+    <?php the_title(); ?>
+    <?php the_excerpt(); ?>
+
+    <?php endwhile; ?>
+    <?php wp_reset_postdata(); ?>
+
+    <?php else : ?>
+    <p><?php __('No News'); ?></p>
+    <?php endif; ?>
+
     return $Content;
 }
 
