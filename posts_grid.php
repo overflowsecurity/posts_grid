9515614d830d82daf6e4358c992a45054fcecd24
Justin Tharpe <justin@Justins-MacBook-Pro.local>
2020-04-09 21:25:03 -0400

    missing .

--- posts_grid.php
+++ posts_grid.php
@@ -22,11 +22,11 @@
         $output .= '<ul>';
         foreach ( $my_posts as $p ){
             $tumbs = get_the_post_thumbnail($p->ID, 'medium');
-            $output = '<div id="leftcolumn">';
+            $output .= '<div id="leftcolumn">';
             $output .= ' <li class="leftcolumn"><a href="' . get_permalink( $p->ID ) . '">' . $p->post_title . '</a></li>';
-            $output = '<div id="content">';
-            $output .= ' <li class="content">' . $tumbs . '</li>';
             $output .= '</div>';
+            $output .= '<div id="content">';
+            $output .= ' <li class="content">' . $tumbs . '</li>';
             $output .= '</div>';
         }
         $output .= '<ul>';
