160ed9dcf80bee2206a02ce3840dbb92d37dedb5
Justin Tharpe <justin@Justins-MacBook-Pro.local>
2020-04-09 21:22:53 -0400

    css test

--- posts_grid.php
+++ posts_grid.php
@@ -22,8 +22,12 @@
         $output .= '<ul>';
         foreach ( $my_posts as $p ){
             $tumbs = get_the_post_thumbnail($p->ID, 'medium');
+            $output = '<div id="leftcolumn">';
             $output .= ' <li class="leftcolumn"><a href="' . get_permalink( $p->ID ) . '">' . $p->post_title . '</a></li>';
+            $output .= '</div>';
+            $output = '<div id="content">';
             $output .= ' <li class="content">' . $tumbs . '</li>';
+            $output .= '</div>';
         }
         $output .= '<ul>';
         $output .= '</div>';
